5e10b1f3f0dcc5836bd3b1ef724237bc70d511d5
Dipirona537 <dipirona28@gmail.com>
2022-02-06 02:25:06 -0400

    testando apenas

--- Formulas/Calculadora.cpp
+++ Formulas/Calculadora.cpp
@@ -131,19 +131,16 @@ int main(int)
 		{
 		triangulo4:
 			printf("funcionando");
+			printf("funcionand2");
 			return 0;
 		}
 		{
 		triangulo5:
-			printf("funcionando");
+			printf("funcionando2");
 			return 0;
 		}
 		return 0;
 	}
-
-
-
-
 
 
 
